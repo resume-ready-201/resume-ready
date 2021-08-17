4e89ac4019c33e52ba0f45a46c5e77525a8c14ae
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-08-17 09:37:51 -0500

    commit on main

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 // sanity test
-console.log("peanut butter and jelly")
+//console.log("peanut butter and jelly")
+
+class cookiePictures
+{
+    // constructor function for the ENTIRE project.
+
+    clicks = 0; //amount of clicks
+    timeShown = 0; // amount of times of image
+
+    // Initial constructor (Must be named CONSTUCTOR)
+    constructor (name , imageSrc)
+    {
+        this.name = name; // image name
+        this.imageSrc = imageSrc; // the source to the image
+    };
+}
+
+//my variables
+let leftImageOnThePage = null;
+let middleImageOnThePage = null;
+let rightImageOnThePage = null;
+let totalClicks = 0;
+const MAX_CLICKS_ALLOWED = 15;
+
+//pictures
+let AllcookiePictures = 
+{
+    new: busmallPictures('white-chocolate-macadamia-nut-cookies', './pictures/white-chocolate-macadamia-nut-cookies.jpg'),
+    new: busmallPictures('Snickerdoodle-Cookie-Recipe', './pictures/Snickerdoodle-Cookie-Recipe.jpg '),
+    new: busmallPictures('Chocolate_Chip_Cookie', './pictures/Chocolate_Chip_Cookie.jpg'),
+};
